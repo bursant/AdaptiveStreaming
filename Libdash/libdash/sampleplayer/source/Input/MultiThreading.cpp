6f891d05210d45870631b34b7ef7c951b923f2ee
bursant <bursant@gmail.com>
2014-03-24 21:29:16 +0100

    Presentation for first seminary

--- Libdash/libdash/sampleplayer/source/Input/MultiThreading.cpp
+++ Libdash/libdash/sampleplayer/source/Input/MultiThreading.cpp
@@ -21,88 +21,11 @@ THREAD_HANDLE   CreateThreadPortable    (void *(*start_routine) (void *), void *
         return th;
     #endif
 }
-void            DestroyThreadPortable   (THREAD_HANDLE th)
+
+void DestroyThreadPortable (THREAD_HANDLE handle)
 {
     #if !defined _WIN32 && !defined _WIN64
-        if(th)
-            free(th);
+        if(handle)
+            free(handle);
     #endif
 }
-
-/****************************************************************************
-* Condition variables for Windows XP and older windows sytems
-*****************************************************************************/
-#if defined WINXPOROLDER
-    void InitCondition       (condition_variable_t *cv)
-    {
-        InitializeCriticalSection(&cv->waitersCountLock);
-
-        cv->waitersCount        = 0;
-        cv->waitGenerationCount = 0;
-        cv->releaseCount        = 0;
-
-        cv->waitingEvent = CreateEvent (NULL,  // no security
-                                        TRUE,  // manual-reset
-                                        FALSE, // non-signaled initially
-                                        NULL); // unnamed
-    }
-    void WaitCondition       (condition_variable_t *cv, CRITICAL_SECTION *externalMutex)
-    {
-        EnterCriticalSection(&cv->waitersCountLock);
-
-        cv->waitersCount++;
-
-        int currentGenerationCount = cv->waitGenerationCount;
-
-        LeaveCriticalSection(&cv->waitersCountLock);
-        LeaveCriticalSection(externalMutex);
-
-        bool isWaitDone = false;
-        while(!isWaitDone)
-        {
-            WaitForSingleObject (cv->waitingEvent, INFINITE);
-            EnterCriticalSection (&cv->waitersCountLock);
-
-            isWaitDone = (cv->releaseCount > 0 && cv->waitGenerationCount != currentGenerationCount);
-            LeaveCriticalSection (&cv->waitersCountLock);
-        }
-
-        EnterCriticalSection(externalMutex);
-        EnterCriticalSection(&cv->waitersCountLock);
-
-        cv->waitersCount--;
-        cv->releaseCount--;
-        bool isLastWaiter = (cv->releaseCount == 0);
-
-        LeaveCriticalSection(&cv->waitersCountLock);
-
-        if(isLastWaiter)
-            ResetEvent(cv->waitingEvent);
-    }
-    void SignalCondition     (condition_variable_t *cv)
-    {
-        EnterCriticalSection(&cv->waitersCountLock);
-
-        if(cv->waitersCount > cv->releaseCount) 
-        {
-            SetEvent(cv->waitingEvent);
-            cv->releaseCount++;
-            cv->waitGenerationCount++;
-        }
-
-        LeaveCriticalSection(&cv->waitersCountLock);
-    }
-    void BroadcastCondition  (condition_variable_t *cv)
-    {
-        EnterCriticalSection(&cv->waitersCountLock);
-
-        if(cv->waitersCount > 0) 
-        {
-            SetEvent(cv->waitingEvent);
-            cv->releaseCount = cv->waitersCount;
-            cv->waitGenerationCount++;
-        }
-
-        LeaveCriticalSection(&cv->waitersCountLock);
-}
-#endif
