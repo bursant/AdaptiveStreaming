cf2f93aa70dc30e289e46dd952e6deed458288ff
bursant <bursant@gmail.com>
2014-04-23 13:13:35 +0200

    Working boost logger.

--- libdash/clientdash/ClientDASH.cpp
+++ libdash/clientdash/ClientDASH.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "libdash.h"
 #include "IDASHManager.h"
-#include "Logger.h"
 #include "Downloader.h"
 #include "Buffer.h"
 #include "Adaptation.h"
@@ -12,22 +11,45 @@
 #include <boost\program_options.hpp>
 #include <boost\algorithm\string\join.hpp>
 
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+
+namespace logs = boost::log;
+namespace src = boost::log::sources;
+namespace keywords = boost::log::keywords;
+namespace po = boost::program_options;
+namespace sinks = boost::log::sinks;
+
 using namespace std;
 using namespace dash;
+using namespace boost::log::trivial;
 
 int main(int argc, char** argv)
 {
 	string url;
 	int bufferSize;
 
-	ILogger* logger = new Logger("clientdash.log");
-	logger->log("Logger initialized.");
+	logs::add_file_log
+    (
+        keywords::file_name = "clientdash.log",                                       
+        keywords::rotation_size = 100 * 1024 * 1024,                                  
+        keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0), 
+        keywords::format = "[%TimeStamp%]: %Message%"                                 
+    );
 
-	namespace po = boost::program_options;
+	logs::add_common_attributes();
+	src::severity_logger< severity_level > lg;
+
 	po::options_description desc("Allowed options");
 	desc.add_options()
 		("help", "produce help message")
-		("url", po::value<string>(&url)->default_value("http://15.0.0.6/bunny_Desktop.mpd"), "specify mpd url")
+		("url", po::value<string>(&url), "specify mpd url")
 		("buffer", po::value<int>(&bufferSize)->default_value(30), "specify buffer size")
 	;
 
@@ -38,27 +60,24 @@ int main(int argc, char** argv)
 	if(vm.count("help"))
 	{
 		cout<<desc<<endl;
-		logger->log("Help printed.");
+		BOOST_LOG_SEV(lg, info) << "Help printed.";
 		return 1;
 	}
 
-	cout<<url<<endl;
-	cout<<bufferSize<<endl;
-
 	IDASHManager* dashManager = CreateDashManager();
-	logger->log("DASH Manager created.");
+	BOOST_LOG_SEV(lg, info) << "DASH Manager created.";
 
 	IBuffer* buffer = new Buffer(bufferSize);
-	logger->log("Buffer created.");
+	BOOST_LOG_SEV(lg, info) << "Thread-safe buffer created.";
 
 	IMPD* mpd = dashManager->Open(strdup(url.c_str()));
-	logger->log("MPD file parsed.");
+	BOOST_LOG_SEV(lg, info) << "MPD file parsed.";
 
 	IAdaptation* adaptation = new Adaptation(mpd);
-	logger->log("Adaptation created.");
+	BOOST_LOG_SEV(lg, info) << "Adaptation created.";
 
 	IDownloader* downloader = new Downloader(buffer, mpd, dashManager, adaptation);
-	logger->log("Downloader started.");
+	BOOST_LOG_SEV(lg, info) << "Downloader started.";
 
 	SDLRenderer *renderer = new SDLRenderer();
 	LibavDecoder *decoder = new LibavDecoder(downloader);
